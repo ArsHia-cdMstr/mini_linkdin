967940a6a046887dd8efed654a1d92bb91fe4179
arshia.mkh <arshia.mokhlesi@gmail.com>
2024-06-16 13:12:58 +0330

    migrate the custom user model

--- linkdin_mini/User_data/models.py
+++ linkdin_mini/User_data/models.py
@@ -2,14 +2,14 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class CustomUser(AbstractUser):
-    fullName = models.CharField(max_length=128)
-    age = models.IntegerField()
-    bio = models.CharField(max_length=256)
-    birthDate = models.DateField()
+    fullName = models.CharField(max_length=128, blank=True, null=True)
+    age = models.IntegerField( blank=True, null=True)
+    bio = models.CharField(max_length=256, blank=True, null=True)
+    birthDate = models.DateField( blank=True, null=True)
     GENDER_CHOICES = [
         ('male', 'Male'),
         ('female', 'Female'),
     ]
-    gender = models.CharField(max_length=6, choices=GENDER_CHOICES)
+    gender = models.CharField(max_length=6, choices=GENDER_CHOICES, blank=True, null=True)
     profile_image = models.ImageField(upload_to='profile_images/', blank=True, null=True)
     profile_header_image = models.ImageField(upload_to='profile_headers/', blank=True, null=True)
