fb5ac971aa6070915c65f1b940e9156db740447e
arshia.mkh <arshia.mokhlesi@gmail.com>
2024-06-16 13:10:28 +0330

    add the custom user model to models.py and  edit setting.py for custom user model

--- linkdin_mini/User_data/models.py
+++ linkdin_mini/User_data/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    fullName = models.CharField(max_length=128)
+    age = models.IntegerField()
+    bio = models.CharField(max_length=256)
+    birthDate = models.DateField()
+    GENDER_CHOICES = [
+        ('male', 'Male'),
+        ('female', 'Female'),
+    ]
+    gender = models.CharField(max_length=6, choices=GENDER_CHOICES)
+    profile_image = models.ImageField(upload_to='profile_images/', blank=True, null=True)
+    profile_header_image = models.ImageField(upload_to='profile_headers/', blank=True, null=True)
